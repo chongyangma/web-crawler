055247cd79ea6902be976689a549add6948b0d45
Chongyang Ma <machy85@gmail.com>
2018-04-14 20:49:42 -0700

    rename and fix for python3

--- scripts/simple.py
+++ scripts/simple.py
@@ -1,8 +1,11 @@
-import urllib2
+try:
+    import urllib.request as urllib2
+except ImportError:
+    import urllib2
 
 def download(url, user_agent='wswp', proxy=None, num_retries=2):
     """Download function with support for proxies"""
-    print 'Downloading:', url
+    print('Downloading:', url)
     headers = {'User-agent': user_agent}
     request = urllib2.Request(url, headers=headers)
     opener = urllib2.build_opener()
@@ -12,7 +15,7 @@ def download(url, user_agent='wswp', proxy=None, num_retries=2):
     try:
         html = opener.open(request).read()
     except urllib2.URLError as e:
-        print 'Download error:', e.reason
+        print('Download error:', e.reason)
         html = None
         if num_retries > 0:
             if hasattr(e, 'code') and 500 <= e.code < 600:
@@ -21,4 +24,4 @@ def download(url, user_agent='wswp', proxy=None, num_retries=2):
     return html
 
 if __name__ == "__main__":
-    print download('http://chongyangma.com')
+    print(download('http://chongyangma.com'))
