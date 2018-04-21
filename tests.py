31557f6bae6af3d9a79cee1587cd91874fb0cf81
Chongyang Ma <chongyangma@users.noreply.github.com>
2018-04-20 23:12:11 -0700

    add build test (#1)
    * add test scripts
    
    * update tests.py
    
    * debug
    
    * fix
    
    * install lxml
    
    * update
    
    * add python 3.6

---
+++ tests.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.insert(0, './scripts')
+
+import link_crawler
+import scrape_callback
+from scrape_callback import ScrapeCallback
+
+if __name__ == '__main__':
+    link_crawler.link_crawler('http://example.webscraping.com',
+                              '/(places/default/index)',
+                              delay=1, num_retries=1, max_depth=1, user_agent='GoodCrawler')
+    scrape_callback.link_crawler('http://example.webscraping.com/',
+                                 '/(places/default/index)',
+                                 scrape_callback=ScrapeCallback())
